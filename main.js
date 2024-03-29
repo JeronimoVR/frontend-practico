96742524fc1256643ada47fc2b32559ff8ec2d26
Jeronimo Velez Rojas <velezrojasjeronimo@gmail.com>
2024-03-28 23:28:33 -0500

    Fucion de product detail

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+shoppingCart.addEventListener("click", toggleProductDetail);
 
 function toggleDesktopMenu() {
+    const isProductDetailClosed = productDetail.classList.contains("inactive");
+
+    if (!isProductDetailClosed) {
+        productDetail.classList.add("inactive");
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+    const isProductDetailClosed = productDetail.classList.contains("inactive");
+
+    if (!isProductDetailClosed) {
+        productDetail.classList.add("inactive");
+    }
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleProductDetail() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+    if (!isMobileMenuClosed || !isDesktopMenuClosed) {
+        mobileMenu.classList.add("inactive");
+        desktopMenu.classList.add("inactive");
+    }
+    productDetail.classList.toggle("inactive");
 }
